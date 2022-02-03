0f4181a19573e5e6c0dba5d253e2eed80bcc0aa4
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-02-03 13:32:50 -0800

    Update roboticGameEngine.js

--- roboticGameEngine.js
+++ roboticGameEngine.js
@@ -29,16 +29,14 @@ var cursor = {
   y: 0,
 }
 canvas.addEventListener("mousemove", updateCursorPos(event)){
-  cursor = getMousePos(canvas, event);
-}
-function  getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect(), // abs. size of element
-      scaleX = canvas.width / rect.width,    // relationship bitmap vs. element for X
-      scaleY = canvas.height / rect.height;  // relationship bitmap vs. element for Y
-
-  return {
-    x: (evt.clientX - rect.left) * scaleX,   // scale mouse coordinates after they have
-    y: (evt.clientY - rect.top) * scaleY     // been adjusted to be relative to element
+  cursor = getMousePos(canvas, event){
+    var rect = canvas.getBoundingClientRect(),
+      scaleX = canvas.width / rect.width,
+      scaleY = canvas.height / rect.height;
+    return {
+      x: (event.clientX - rect.left) * scaleX,
+      y: (event.clientY - rect.top) * scaleY
+    }
   }
 }
 
