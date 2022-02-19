91283429bdccea83c2cf7c9174b779e3dd80406a
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2022-02-19 11:04:26 -0800

    more testing

--- addFunctions.js
+++ addFunctions.js
@@ -1,7 +1,9 @@
 const folder = './functions/';
-const fs = require('fs');
-const script = document.createElement('script')
+var fs = require('fs');
+const script = document.createElement('script');
+var files = fs.readdirSync('./functions/');
 
+console.log(files)
 fs.readdir(folder, (err, files) => {
   files.forEach(file => {
     script.src = folder + file;
