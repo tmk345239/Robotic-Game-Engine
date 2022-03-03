eba81edad46cbcfb3a1def8b577aef9658f93b82
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-03-03 14:32:21 -0800

    added RTA code

--- functions/rta.js
+++ functions/rta.js
@@ -6,16 +6,33 @@ function rta(type, rotate_or_x, speed_or_y){
             return "0:Rotation to Axis, 1:Axis to Rotation";
         break;
         case 0:
+            var r = rotate_or_x;
+            var s = speed_or_y;
             //angle to axis
             var output = {
                 xMove: 0,
                 yMove: -0,
             }
-            
+            //rotate to radins function here
+            output.xMove = s * Math.acos(r);
+            output.yMove = s * Math.asin(r);
+            output.yMove = -output.yMove;
+
+            return(output);
         break;
         case 1:
+            var x = rotate_or_x;
+            var y = speed_or_y;
             //axis to angle
-
+            var output = {
+                angle: 0,
+                speed: 0,
+            }
+            output.angle = Math.tan(y/x);
+            x = x * x
+            y = y * y
+            output.speed = Math.sqrt(x+y);
+            return(output);
         break;
     }
 }
