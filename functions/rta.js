258339105164a09c09df850123cf5e9956f8c6e9
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-03-03 13:45:51 -0800

    started on RTA function

--- functions/rta.js
+++ functions/rta.js
@@ -1,5 +1,21 @@
 //RTA: Rotation To Axis
 
 function rta(type, rotate_or_x, speed_or_y){
+    switch(type){
+        default:
+            return "0:Rotation to Axis, 1:Axis to Rotation";
+        break;
+        case 0:
+            //angle to axis
+            var output = {
+                xMove: 0,
+                yMove: -0,
+            }
+            
+        break;
+        case 1:
+            //axis to angle
 
+        break;
+    }
 }
