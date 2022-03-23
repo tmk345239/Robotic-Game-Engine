426b2c98ba991d5bd6a9b67cea55a47cb190e860
tmk345239 <97308927+tmk345239@users.noreply.github.com>
2022-03-23 13:32:57 -0700

    finished wall collisions.

--- functions/movements/move.js
+++ functions/movements/move.js
@@ -8,7 +8,20 @@ function move(id){
         for (var id2=0; id2 < hitbox.tag.length; id2++){//check if x hit
             if(hitbox.stopMove[id2]){
                 if(hitCheck(id, id2)){
-                    hitbox.x[id] -= hitbox.xMove[id];//undo move
+                    //hitbox.x[id] -= hitbox.xMove[id];//undo move
+                    switch(Math.sign(hitbox.xMove[id]) == 1){
+                        case -1:
+                            var dis = hitbox.farX[id2] - hitbox.x[id] + 1;
+                            hitbox.x[id] += dis;
+                            break;
+                        case 0:
+                            break;
+                        case 1:
+                            var dis = hitbox.x[id2] - hitbox.farX[id] - 1;
+                            hitbox.x[id] -= dis;
+                            break;
+                    }
+                    hitbox.xMove[id] = 0;
                 }
             }
         }
@@ -17,10 +30,20 @@ function move(id){
         for (var id2=0; id2 < hitbox.tag.length; id2++){//check if y hit
             if(hitbox.stopMove[id2]){
                 if(hitCheck(id, id2)){
-                    hitbox.y[id] -= hitbox.yMove[id];//undo move
-                    if(Math.sign(hitbox.yMove[id]) == 1){
-
+                    //hitbox.y[id] -= hitbox.yMove[id];//undo move
+                    switch(Math.sign(hitbox.yMove[id]) == 1){
+                        case -1:
+                            var dis = hitbox.farY[id2] - hitbox.y[id] + 1;
+                            hitbox.y[id] += dis;
+                            break;
+                        case 0:
+                            break;
+                        case 1:
+                            var dis = hitbox.y[id2] - hitbox.farY[id] - 1;
+                            hitbox.y[id] -= dis;
+                            break;
                     }
+                    hitbox.yMove[id] = 0;
                 }
             }
         }
